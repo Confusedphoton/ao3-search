f60d57a12c9a150c7e35b1b082683816697637ed
Confusedphoton <confusedphoton@gmail.com>
2026-06-20 23:03:17 -0400

    Fixed inclusion of datadump in extension build

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'wxt';
 
 export default defineConfig({
   manifestVersion: 3,
+  zip: {
+    // Firefox zips project sources by default; exclude local AO3 dataset files.
+    excludeSources: ['**/*.csv', '20210226-stats/**'],
+  },
   manifest: {
     name: 'AO3 Semantic Search',
     description:
